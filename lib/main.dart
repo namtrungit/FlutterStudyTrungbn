2f9557a15617143f69336c0404fd1cdbe23cf513
TrungBN <trungbn@bestarion.com>
2025-06-01 22:25:46 +0700

    Finish Build a Layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,20 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: const Text(appTitle)),
         body: const SingleChildScrollView(
-          child: Column(children: [Text('Line 1'), Text('Line 2')]),
+          child: Column(
+            children: [
+              ImageSection(image: 'images/lake.jpg'),
+              TitleSection(name: 'TrungBN', location: 'Quan 4'),
+              ButtonSection(),
+              TextSection(
+                description:
+                    'TrungBN HAHAHA TrungBN HAHAHA TrungBN HAHAHA TrungBN HAHAHA'
+                    'TrungBN HAHAHA TrungBN HAHAHA TrungBN HAHAHA TrungBN HAHAHA'
+                    'TrungBN HAHAHA TrungBN HAHAHA TrungBN HAHAHA TrungBN HAHAHA'
+                    'TrungBN HAHAHA TrungBN HAHAHA TrungBN HAHAHA TrungBN HAHAHA',
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -52,5 +65,82 @@ class TitleSection extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(color: color, icon: Icons.call, label: 'CALL'),
+          ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
+          ButtonWithText(color: color, icon: Icons.share, label: 'SHARE'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key, required this.description});
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(description, softWrap: true),
+    );
+  }
+}
+
+class ImageSection extends StatelessWidget {
+  const ImageSection({super.key, required this.image});
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(image, width: 600, height: 240, fit: BoxFit.cover);
   }
 }
