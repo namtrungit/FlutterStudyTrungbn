588588ea48214f52a367e47196eea989f652f567
TrungBN <trungbn@bestarion.com>
2025-06-08 21:45:32 +0700

    Create StateFull Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,337 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// ParentWidget manages the state for TapboxB.
 
-const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
-class Location {
-  const Location({
-    required this.name,
-    required this.country,
-    required this.imageUrl,
-  });
+//------------------------ ParentWidget --------------------------------
 
-  final String name;
-  final String country;
-  final String imageUrl;
-}
-const urlPrefix =
-    'https://docs.flutter.dev/cookbook/img-files/effects/parallax';
-const locations = [
-  Location(
-    name: 'Mount Rushmore',
-    country: 'U.S.A',
-    imageUrl: '$urlPrefix/01-mount-rushmore.jpg',
-  ),
-  Location(
-    name: 'Gardens By The Bay',
-    country: 'Singapore',
-    imageUrl: '$urlPrefix/02-singapore.jpg',
-  ),
-  Location(
-    name: 'Machu Picchu',
-    country: 'Peru',
-    imageUrl: '$urlPrefix/03-machu-picchu.jpg',
-  ),
-  Location(
-    name: 'Vitznau',
-    country: 'Switzerland',
-    imageUrl: '$urlPrefix/04-vitznau.jpg',
-  ),
-  Location(
-    name: 'Bali',
-    country: 'Indonesia',
-    imageUrl: '$urlPrefix/05-bali.jpg',
-  ),
-  Location(
-    name: 'Mexico City',
-    country: 'Mexico',
-    imageUrl: '$urlPrefix/06-mexico-city.jpg',
-  ),
-  Location(name: 'Cairo', country: 'Egypt', imageUrl: '$urlPrefix/07-cairo.jpg'),
-];
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  final List<String> items = List<String>.generate(100, (i) => 'Item $i');
+class ParentWidget extends StatefulWidget {
+  const ParentWidget({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    const title = 'Floating App Bar';
-
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
-      title: title,
-      home: Scaffold(
-        body: Center(child: ParallaxRecipe(locations: locations)),
-      ),
-    );
-  }
+  State<ParentWidget> createState() => _ParentWidgetState();
 }
 
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
 
-
-class ParallaxRecipe extends StatelessWidget {
-  const ParallaxRecipe({super.key, required this.locations});
-
-  final List<Location> locations;
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          for (final location in locations)
-            LocationListItem(
-              imageUrl: location.imageUrl,
-              name: location.name,
-              country: location.country,
-            ),
-        ],
-      ),
-    );
-  }
-}
-
-@immutable
-class LocationListItem extends StatelessWidget {
-  const LocationListItem({
-    super.key,
-    required this.imageUrl,
-    required this.name,
-    required this.country,
-  });
-
-  final String imageUrl;
-  final String name;
-  final String country;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-      child: AspectRatio(
-        aspectRatio: 16 / 9,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: Stack(
-            children: [
-              _buildParallaxBackground(context),
-              _buildGradient(),
-              _buildTitleAndSubtitle(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildParallaxBackground(BuildContext context) {
-    return Positioned.fill(child: Image.network(imageUrl, fit: BoxFit.cover));
-  }
-
-  Widget _buildGradient() {
-    return Positioned.fill(
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.transparent, Colors.black.withValues(alpha: 0.7)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: const [0.6, 0.95],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTitleAndSubtitle() {
-    return Positioned(
-      bottom: 20,
-      left: 20,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            name,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            country,
-            style: const TextStyle(color: Colors.white, fontSize: 14),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ItemWidget extends StatelessWidget {
-  const ItemWidget({super.key, required this.text});
-
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: SizedBox(height: 100, child: Center(child: Text(text))),
-    );
-  }
-}
-
-abstract class ListItem {
-  Widget buildTitle(BuildContext context);
-  Widget buildSubtitle(BuildContext context);
-}
-
-class HeadingItem implements ListItem {
-  final String heading;
-  HeadingItem(this.heading);
-  @override
-  Widget buildTitle(BuildContext context) {
-    return Text(heading, style: Theme.of(context).textTheme.titleLarge);
-  }
-
-  @override
-  Widget buildSubtitle(BuildContext context) {
-    return const SizedBox.shrink();
-  }
-}
-
-class MessageItem implements ListItem {
-  final String sender;
-  final String body;
-  MessageItem(this.sender, this.body);
-  @override
-  Widget buildTitle(BuildContext context) {
-    return Text(sender);
-  }
-
-  @override
-  Widget buildSubtitle(BuildContext context) {
-    return Text(body);
-  }
-}
-
-final items = List<ListItem>.generate(
-  1000,
-  (index) => index % 6 == 0
-      ? HeadingItem('Heading $index')
-      : MessageItem('Sender $index', 'Messsage body $index'),
-);
-
-class TitleSection extends StatelessWidget {
-  const TitleSection({super.key, required this.name, required this.location});
-
-  final String name;
-  final String location;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    name,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(location, style: TextStyle(color: Colors.grey[500])),
-              ],
-            ),
-          ),
-          Icon(Icons.star, color: Colors.red[500]),
-          const Text('41'),
-        ],
-      ),
-    );
-  }
-}
-
-class ButtonSection extends StatelessWidget {
-  const ButtonSection({super.key});
-  @override
-  Widget build(BuildContext context) {
-    final Color color = Theme.of(context).primaryColor;
     return SizedBox(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ButtonWithText(color: color, icon: Icons.call, label: 'CALL'),
-          ButtonWithText(color: color, icon: Icons.near_me, label: 'ROUTE'),
-          ButtonWithText(color: color, icon: Icons.share, label: 'SHARE'),
-        ],
-      ),
+      child: TapboxB(active: _active, onChanged: _handleTapboxChanged),
     );
   }
 }
 
-class ButtonWithText extends StatelessWidget {
-  const ButtonWithText({
-    super.key,
-    required this.color,
-    required this.icon,
-    required this.label,
-  });
+//------------------------- TapboxB ----------------------------------
 
-  final Color color;
-  final IconData icon;
-  final String label;
+class TapboxB extends StatelessWidget {
+  const TapboxB({super.key, this.active = false, required this.onChanged});
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Icon(icon, color: color),
-        Padding(
-          padding: const EdgeInsets.only(top: 8),
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
           child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
+            active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32, color: Colors.white),
           ),
         ),
-      ],
+      ),
     );
-  }
-}
-
-class TextSection extends StatelessWidget {
-  const TextSection({super.key, required this.description});
-
-  final String description;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Text(description, softWrap: true),
-    );
-  }
-}
-
-class ImageSection extends StatelessWidget {
-  const ImageSection({super.key, required this.image});
-
-  final String image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Image.asset(image, width: 600, height: 240, fit: BoxFit.cover);
   }
 }
